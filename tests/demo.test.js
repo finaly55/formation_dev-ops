5b8eb7c17f1d18ff1c58485020d255113939c67b
Julien Lebatard <julien.lbt1@gmail.com>
2020-11-27 14:20:00 +0100

    ajout d'un test failed

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,11 +1,19 @@
 var should = require("should");
-var User = function(name){
- this.name = name;
+var User = function (name) {
+    this.name = name;
 };
-describe("Checking if the user is created correctly", function(){
- it("should create the user with the correct name", function(){
- debugger
- var tom = new User("tom");
- tom.name.should.be.equal("tom");
- });
+describe("Checking if the user is created correctly", function () {
+    it("should create the user with the correct name", function () {
+        debugger
+        var tom = new User("tom");
+        tom.name.should.be.equal("tom");
+    });
+});
+
+describe("Checking if the user is created correctly", function () {
+    it("should create the user with the correct name", function () {
+        debugger
+        var tom = new User("tom");
+        tom.name.should.be.equal("tomma");
+    });
 });
